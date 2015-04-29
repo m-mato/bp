ee6ec2b769b04e013067eb9c4215781d779cb7a4
m-mato <mato.majdis@gmail.com>
2015-04-29 14:56:46 +0200

    priklad  code-improvement upravy

---
+++ examples/get_&_set/code-improvement/src/main/java/application/AccCreator.java
@@ -0,0 +1,34 @@
+package application;
+
+/**
+ *
+ * @author Matej Majdis
+ */
+public class AccCreator {
+
+    public static String makeGetTemplate(String fieldName, String fieldType, String returnType) {
+        final String upName = fieldName.toUpperCase();
+        final String header = "public static " + fieldType + " get" + upName + "StatGenerated(Object obj)";
+        final String bodyRetype = returnType + " ret = (" + returnType + ") obj;";
+        final String bodyReturn = "return ret." + fieldName + ";";
+        final String body = bodyRetype + "\n" + "\n" + bodyReturn;
+
+        final String getTemplate = header + " {\n" + body + "\n}";
+
+        return getTemplate;
+    }
+
+    public static String makeSetTemplate(String fieldName, String fieldType, String parameterType) {
+        final String upName = fieldName.toUpperCase();
+        final String header = "public static void set" + upName + "StatGenerated(Object obj, " + fieldType + " " + fieldName + ")";
+        final String bodyRetype = parameterType + " t = (" + parameterType + ") obj;";
+        final String bodySet = "t." + fieldName + " = " + fieldName + ";";
+        final String body = bodyRetype + bodySet;
+
+        final String setTemplate = header + " {\n" + body + "\n}";
+
+        return setTemplate;
+
+    }
+
+}
